b4f247e7ad91a05097019619af30d18da2e580a0
Jason Hansen <jason_hansen@outlook.com>
2018-06-25 22:21:00 -0700

    [authorization] step 2, hide delete link

--- src/components/DeleteLink.js
+++ src/components/DeleteLink.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UserCanDeleteTweet from '../decorators/UserCanDeleteTweet';
 
+@UserCanDeleteTweet
 class DeleteLink extends React.Component {
 
   static propTypes = {
