b7e02aa91fbf9277236e06f3b01f522d1c41a43f
Jason Hansen <jason_hansen@outlook.com>
2018-06-25 22:10:43 -0700

    [infinite-scrolling] step 3, convert the feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,108 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'lore-hook-connect';
-import { Link } from 'react-router';
-import PayloadStates from '../constants/PayloadStates';
+import _ from 'lodash';
+import InfiniteScrollingList from './InfiniteScrollingList';
 import Tweet from './Tweet';
 
-@connect(function(getState, props) {
-  const { location } = props;
-
-  return {
-    tweets: getState('tweet.find', {
-      pagination: {
-        sort: 'createdAt DESC',
-        page: location.query.page || '1'
-      }
-    })
-  };
-})
 class Feed extends React.Component {
 
-  static propTypes = {
-    tweets: PropTypes.object.isRequired
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      tweets: props.tweets,
-      nextTweets: props.tweets
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const nextTweets = nextProps.tweets;
-
-    if (nextTweets.state === PayloadStates.FETCHING) {
-      this.setState({
-        nextTweets: nextTweets,
-        isFetching: true
-      });
-    } else {
-      this.setState({
-        tweets: nextTweets,
-        nextTweets: nextTweets,
-        isFetching: false
-      });
-    }
-  }
-
-  renderTweet(tweet) {
-    return (
-      <Tweet key={tweet.id} tweet={tweet} />
-    );
-  }
-
-  renderPaginationLink(page, currentPage) {
-    return (
-      <li key={page} className={currentPage === String(page) ? 'active' : ''}>
-        <Link to={{ pathname: '/', query: { page: page } }}>
-          {page}
-        </Link>
-      </li>
-    );
-  }
-
   render() {
-    const { tweets, nextTweets } = this.state;
-    const currentPage = nextTweets.query.pagination.page;
-    const paginationLinks = [];
-
-    // check if we're fetching the next page of tweets
-    const isFetchingNextTweets = nextTweets.state === PayloadStates.FETCHING;
-
-    if (tweets.state === PayloadStates.FETCHING) {
-      return (
-        <div className="feed">
-          <h2 className="title">
-            Feed
-          </h2>
-          <div className="loader"/>
-        </div>
-      );
-    }
-
-    // calculate the number of pagination links from our metadata, then
-    // generate an array of pagination links
-    const numberOfPages = Math.ceil(tweets.meta.totalCount / tweets.meta.perPage);
-    for (let pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
-      paginationLinks.push(this.renderPaginationLink(pageNumber, currentPage));
-    }
-
     return (
       <div className="feed">
         <h2 className="title">
           Feed
         </h2>
-        <ul className={`media-list tweets ${isFetchingNextTweets ? 'transition' : ''}`}>
-          {tweets.data.map(this.renderTweet)}
-        </ul>
-        <nav>
-          <ul className="pagination">
-            {paginationLinks}
-          </ul>
-        </nav>
+        <InfiniteScrollingList
+          select={(getState) => {
+            return getState('tweet.find', {
+              pagination: {
+                sort: 'createdAt DESC',
+                page: 1
+              }
+            });
+          }}
+          row={(tweet) => {
+            return (
+              <Tweet key={tweet.id} tweet={tweet} />
+            );
+          }}
+          refresh={(page, getState) => {
+            return getState('tweet.find', page.query);
+          }}
+          selectNextPage={(lastPage, getState) => {
+            const lastPageNumber = lastPage.query.pagination.page;
+
+            return getState('tweet.find', _.defaultsDeep({
+              pagination: {
+                page: lastPageNumber + 1
+              }
+            }, lastPage.query));
+          }}
+        />
       </div>
     );
   }
