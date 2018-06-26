e4301fa1c45230f24781037bc6ac991d034ec8c8
Jason Hansen <jason_hansen@outlook.com>
2018-06-25 22:05:25 -0700

    [pagination] step 2, paginate the tweets

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,8 +5,15 @@ import PayloadStates from '../constants/PayloadStates';
 import Tweet from './Tweet';
 
 @connect(function(getState, props) {
+  const { location } = props;
+
   return {
-    tweets: getState('tweet.find')
+    tweets: getState('tweet.find', {
+      pagination: {
+        sort: 'createdAt DESC',
+        page: location.query.page || '1'
+      }
+    })
   };
 })
 class Feed extends React.Component {
